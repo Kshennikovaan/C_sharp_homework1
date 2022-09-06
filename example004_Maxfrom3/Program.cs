9df5bcc32ed923ddd348549bb525bef69a5fafd6
Anastasia <plushvochka@gmail.com>
2022-09-06 13:16:58 +0300

    added example004_Maxfrom3

---
+++ example004_Maxfrom3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число A");
+int A = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите натуральное число B");
+int B = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите натуральное число C");
+int C = Convert.ToInt32 (Console.ReadLine());
+
+int max = A;
+
+if(B > max) max = B;
+if(C > max) max = C;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
