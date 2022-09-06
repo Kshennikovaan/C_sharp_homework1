ae2bb42a6818ec1f19a5a30127181473e52aeca4
Anastasia <plushvochka@gmail.com>
2022-09-06 17:15:42 +0300

    added example006_Parity

---
+++ example006_Parity/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int A = Convert.ToInt32 (Console.ReadLine());
+
+if(A %2 == 0)
+{
+    Console.Write("Число является чётным");
+}
+else 
+{
+    Console.Write("Число является нечетным");
+}
