7c1089cabba4d8ba0626b8e9d1ab4f53a032eaea
Anastasia <plushvochka@gmail.com>
2022-09-06 13:11:45 +0300

    added example002_MaxMin

---
+++ example002_MaxMin/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите натуральное число A");
+int A = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите натуральное число B");
+int B = Convert.ToInt32 (Console.ReadLine());
+
+int max = A;
+int min = B;
+
+if(B > A) 
+{
+    max = B;
+    min = A;
+}
+else 
+{
+    max = A;
+    min = B;
+}
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+Console.Write("Минимальное число = ");
+Console.WriteLine(min);
+
